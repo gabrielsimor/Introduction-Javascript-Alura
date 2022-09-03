2340292579b08a9c1da7aaebf3641ca2d3369568
Gabriel Simor <simor.gabriel@gmail.com>
2022-09-03 20:30:48 -0300

    add all archives

---
+++ js/buscar-pacientes.js
@@ -0,0 +1,24 @@
+var botaoAdicionar = document.querySelector('#buscar-pacientes');
+
+botaoAdicionar.addEventListener('click', function(){
+    //http://api-pacientes.herokuapp.com/pacientes
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", 'http://api-pacientes.herokuapp.com/pacientes');
+    xhr.addEventListener('load', function(){
+        
+        if(xhr.status == 200){
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+            pacientes.forEach(function(paciente){
+                adicionaPacienteNaTabela(paciente);
+            })
+        }else{
+            console.log( xhr.status);
+            console.log( xhr.responseText);
+            var erroAjax = document.querySelector('#erro-ajax');
+            erroAjax.classList.remove('invisivel')
+        }       
+    })
+    xhr.send();
+})
